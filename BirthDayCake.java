3599ce28dccb07aa03084e80ca591a5dc75fdc27
edsna <edsonaguiar17@gmail.com>
2020-05-29 15:34:23 -0400

    Adds solution to BirthDay Cake Candles problem

---
+++ BirthDayCake.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        //Instantiate count & tallest candle
+        int count = 0;
+        int tallest = 0;
+        //Loop array
+        for(int i = 0; i < ar.length; i++){
+            if(ar[i] > tallest){    //If element is tallest
+                tallest = ar[i];    //update tallest element
+                count = 1;          //start counting
+            }else if(ar[i] == tallest){ //If there are more tallest candles
+                count += 1;         //update count
+            }
+        }
+        return count;               //return it
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
